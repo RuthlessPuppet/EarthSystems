3bc13560b33a0ddd952ece52263f6d5ff0fd3444
RuthlessPuppet <97858010+RuthlessPuppet@users.noreply.github.com>
2024-04-29 11:29:10 -0700

    It doesnt work :/

--- causes/index.js
+++ causes/index.js
@@ -1,4 +1,5 @@
 //write code to replace  paragraph with with content.txt
 var content = document.getElementById(content)
 
-content.innerHTML() = "<p>Does this work?</p>"
+content.innerHTML() = "<p>Does this work?</p>" //IT DOESNT WORK 
+//insertAdjacentText method
